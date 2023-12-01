bf3e36f9586ac444a42514cb84e6267dae6f6d36
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-01 15:00:02 +0100

    Modified  lib/views/reservation.dart

---
+++ lib/views/reservation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Resersvation extends StatefulWidget {
+  const Resersvation({super.key});
+
+  @override
+  State<Resersvation> createState() => _ResersvationState();
+}
+
+class _ResersvationState extends State<Resersvation> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
