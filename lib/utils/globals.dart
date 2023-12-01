b0ee49f69d26190999352203547d562d06ec8c5e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-01 13:48:03 +0100

    ModifiedM lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 //COLORS
 const Color teal = Color.fromARGB(255, 0, 207, 159);
@@ -6,3 +7,4 @@ const Color gray = Color.fromARGB(255, 51, 56, 66);
 const Color white = Colors.white;
 
 //VARS
+Box? userSettings;
