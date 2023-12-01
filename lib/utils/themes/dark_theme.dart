16d883e7799397957bbcf92c01e0443c9bba051e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-01 14:17:02 +0100

    Modified  lib/utils/themes/dark_theme.dart

---
+++ lib/utils/themes/dark_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: Colors.tealAccent,
+  hintColor: Colors.tealAccent,
+  scaffoldBackgroundColor: Colors.grey[900],
+);
