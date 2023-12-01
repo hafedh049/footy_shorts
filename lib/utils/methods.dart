edfd9020ecc6d4b82029c65cfa31d5ba4cfd70a5
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-01 14:04:42 +0100

    Modified  lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -1,9 +1,10 @@
 import 'package:footy_shorts/utils/globals.dart';
 import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<bool> loadUserLocalSettings() async {
   try {
-    Hive.init(path)
+    Hive.init((await getApplicationDocumentsDirectory()).path);
     userLocalSettings = await Hive.openBox('userLocalSettings');
     return true;
   } catch (e) {
