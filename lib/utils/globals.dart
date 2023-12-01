b2a2c9473d329a35861778d257cea72bf9832363
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-01 13:43:25 +0100

    ModifiedM lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,6 +1,8 @@
-//COLORS
 import 'package:flutter/material.dart';
 
+//COLORS
 const Color teal = Color.fromARGB(255, 0, 207, 159);
 const Color gray = Color.fromARGB(255, 51, 56, 66);
 const Color white = Colors.white;
+
+//VARS
