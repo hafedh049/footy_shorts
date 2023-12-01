994d8bf619933b3dcc0df515e22e6c8122968d66
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-01 14:53:31 +0100

    ModifiedM lib/views/highlight.dart

---
+++ lib/views/highlight.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoHighlight extends StatefulWidget {
+  const VideoHighlight({super.key});
+
+  @override
+  State<VideoHighlight> createState() => _VideoHighlightState();
+}
+
+class _VideoHighlightState extends State<VideoHighlight> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
