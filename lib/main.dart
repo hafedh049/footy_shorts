001e1b816a7990914d89a132774bbe1cf687dc52
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 10:26:52 +0100

    Modified  lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:footy_shorts/home.dart';
-import 'package:footy_shorts/utils/globals.dart';
 import 'package:footy_shorts/utils/methods.dart';
-import 'package:footy_shorts/utils/themes/dark_theme.dart';
-import 'package:footy_shorts/utils/themes/light_theme.dart';
 import 'package:get/get.dart';
 
 Future<void> main() async {
