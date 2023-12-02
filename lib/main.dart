8059dedc324544826914e8ad716453a3f709c6f3
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 10:09:22 +0100

    Modified  lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class Main extends StatelessWidget {
       dark: darkTheme,
       light: lightTheme,
       builder: (ThemeData light, ThemeData dark) {
-        return GetMaterialApp(
+        return MaterialApp(
           theme: light,
           darkTheme: dark,
           debugShowCheckedModeBanner: false,
