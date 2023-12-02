a4917556ec87f5319c564a1f4986c448ffa468e7
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 11:16:29 +0100

    Modified  lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class Main extends StatelessWidget {
           if (snapshot.hasData) {
             return const Home();
           } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold();
           } else {}
         },
       ),
