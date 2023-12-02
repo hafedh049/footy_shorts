3a7ca9e12392317fab1ac55aa713056483f2f328
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 11:55:34 +0100

    Modified  lib/views/auth/sign_in.dart

--- lib/views/auth/sign_in.dart
+++ lib/views/auth/sign_in.dart
@@ -12,7 +12,20 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: <Widget>[],
+        children: <Widget>[
+          InfiniteCarousel.builder(
+            itemCount: 10,
+            itemExtent: 120,
+            center: true,
+            anchor: 0.0,
+            velocityFactor: 0.2,
+            onIndexChanged: (index) {},
+            controller: controller,
+            axisDirection: Axis.horizontal,
+            loop: true,
+            itemBuilder: (context, itemIndex, realIndex) {},
+          )
+        ],
       ),
     );
   }
