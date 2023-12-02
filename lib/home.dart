2eb256972fdbae84c2f840caf032cd6564ded9d2
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 10:15:24 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -43,7 +43,11 @@ class _HomeState extends State<Home> {
           tabBackgroundColor: teal.withOpacity(.1), // selected tab background color
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
           tabs: <GButton>[
-            GButton(icon: Bootstrap.house, text: 'Home'),
+            GButton(
+              icon: Bootstrap.house,
+              text: 'Home',
+              onPressed: () {},
+            ),
             GButton(
               icon: LineIcons.heart_o,
               text: 'Likes',
