02f238cc263571fee2b8f400c44683e6c7e65906
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 12:47:28 +0100

    Modified  lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:footy_shorts/utils/globals.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
@@ -15,14 +16,14 @@ Future<bool> loadUserLocalSettings() async {
 Future<bool> load() async {
   try {
     Firebase.initializeApp(
-          options: const FirebaseOptions(
-            storageBucket: "footy-shorts.appspot.com",
-            apiKey: "AIzaSyBW-rnLPP0cHENo9WRbmwZZNzGP3f1KwkA",
-            appId: "1:731346863322:android:d7391a9de82ddcc03c91e3",
-            messagingSenderId: "731346863322",
-            projectId: "footy-shorts",
-          ),
-        )
+      options: const FirebaseOptions(
+        storageBucket: "footy-shorts.appspot.com",
+        apiKey: "AIzaSyBW-rnLPP0cHENo9WRbmwZZNzGP3f1KwkA",
+        appId: "1:731346863322:android:d7391a9de82ddcc03c91e3",
+        messagingSenderId: "731346863322",
+        projectId: "footy-shorts",
+      ),
+    );
     return true;
   } catch (e) {
     return false;
