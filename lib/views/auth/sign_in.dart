5ba2f623197981b1e3f94ed80f5d5e2f5095acc8
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 12:20:47 +0100

    Modified  lib/views/auth/sign_in.dart

--- lib/views/auth/sign_in.dart
+++ lib/views/auth/sign_in.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:infinite_carousel/infinite_carousel.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
