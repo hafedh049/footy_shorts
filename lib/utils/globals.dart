7a9e3fc11c819c1470f09c168e02733914ca9641
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 09:38:30 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -6,6 +6,7 @@ const Color teal = Color.fromARGB(255, 0, 207, 159);
 const Color blue = Color.fromARGB(255, 0, 128, 178);
 const Color gray = Color.fromARGB(255, 51, 56, 66);
 const Color white = Color.fromARGB(255, 204, 204, 195);
+const Color transparent = Colors.transparent;
 
 //VARS
 Box? userLocalSettings;
