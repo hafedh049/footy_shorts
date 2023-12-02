ea5006f73e342a7282c41c3954c546b955342bd0
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 11:27:46 +0100

    Modified  lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class Main extends StatelessWidget {
             storageBucket: "footy-shorts.appspot.com",
             apiKey: "AIzaSyBW-rnLPP0cHENo9WRbmwZZNzGP3f1KwkA",
             appId: "1:731346863322:android:d7391a9de82ddcc03c91e3",
-            messagingSenderId: "",
+            messagingSenderId: "731346863322	",
             projectId: "footy-shorts",
           ),
         ),
