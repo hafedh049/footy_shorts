301a660850ba59f0f5414e39750571e7d6f6c49f
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 11:25:46 +0100

    Modified  lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class Main extends StatelessWidget {
       home: FutureBuilder<FirebaseApp>(
         future: Firebase.initializeApp(
           options: FirebaseOptions(
+            storageBucket: "",
             apiKey: "AIzaSyBW-rnLPP0cHENo9WRbmwZZNzGP3f1KwkA",
             appId: appId,
             messagingSenderId: messagingSenderId,
