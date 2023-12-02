53e2a0cf2146edc643d0b3c518a7b72e7d3d3bc3
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 11:49:14 +0100

    Modified  lib/views/auth/sign_in.dart

--- lib/views/auth/sign_in.dart
+++ lib/views/auth/sign_in.dart
@@ -12,6 +12,7 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[],
       ),
     );
