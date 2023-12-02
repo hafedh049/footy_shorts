27b5acba9956144261782790d98a0191cc8684d6
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-02 11:15:24 +0100

    Modified  lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class Main extends StatelessWidget {
       home: FutureBuilder<FirebaseApp>(
         future: null,
         builder: (BuildContext context,AsyncSnapshot<FirebaseApp> snapshot) {
-          if(){return const Home();}
+          if(snapshot){return const Home();}
           else if(){}
           else {}
         },
